5a8a2f149794afb5d97fe92246daf5c9a8421f34
alec.schneider <alec.schneider@student.hpi.de>
2016-03-20 13:35:36 +0100

    script to add all emotes of my chat to a package

---
+++ loadEmotePack.js
@@ -0,0 +1,28 @@
+var myID = 103830014;
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+var dataSchema = mongoose.Schema({
+    chat_id: {type: Number, unique: true},
+    emotes: [{phrase: String, data_type: String, file_id: String}]
+});
+
+var Data = mongoose.model('Data', dataSchema);
+
+Data.findOne({chat_id: myID}, 'emotes', function(err, data) {
+    Data.update(
+        {chat_id: 1337},
+        {$pushAll: {emotes: data.emotes},
+        {upsert: true},
+        function (err) {
+            if (err) return console.log(err);
+            });
+    });
+});
